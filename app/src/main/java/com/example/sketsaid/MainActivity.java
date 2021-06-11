ca0d32d609535ae3f19499899f57f387daf49a1e
steven300900 <stevenkristian300900@gmail.com>
2021-06-11 11:34:49 +0700

    Added bottom navigation bar

--- app/src/main/java/com/example/sketsaid/MainActivity.java
+++ app/src/main/java/com/example/sketsaid/MainActivity.java
@@ -1,34 +1,53 @@
 package com.example.sketsaid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.*;
-import android.view.View;
+import android.view.*;
 import com.example.sketsaid.ui.login.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String SEARCH_STRING = null;
+    BottomNavigationView bottomView;
+    Button loginButton;
+    public static final String MOVE = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.page_1:
+                            break;
+                        case R.id.page_2:
+                            moveToArtSearch(this);
+                            break;
+                        case R.id.page_3:
+                            break;
+                    }
+                    return false;
+                }
+            }
+        );
     }
 
-    public void searchCharacter(View view) {
-        Intent intent = new Intent(this, commission.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(SEARCH_STRING, message);
+    public void moveToArtSearch(BottomNavigationView.OnNavigationItemSelectedListener view) {
+        Intent intent = new Intent(this, Commission.class);
+        intent.putExtra(MOVE, "");
         startActivity(intent);
     }
 
-    public void login(View view) {
+    public void moveToLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(SEARCH_STRING, message);
+        intent.putExtra(MOVE, "Login");
         startActivity(intent);
     }
 }
