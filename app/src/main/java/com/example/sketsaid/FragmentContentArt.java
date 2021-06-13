5c894bd0b7a7ebfc55aa3996b62f72069a4df8b7
steven300900 <stevenkristian300900@gmail.com>
2021-06-13 15:36:20 +0700

    Implements Vertical Scrolling

--- app/src/main/java/com/example/sketsaid/FragmentContentArt.java
+++ app/src/main/java/com/example/sketsaid/FragmentContentArt.java
@@ -6,12 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
-public class FragmentContentSlider extends Fragment {
-    public FragmentContentSlider(){
-        super(R.layout.fragment_slides);
+public class FragmentContentArt extends Fragment {
+    public FragmentContentArt(){
+        super(R.layout.fragment_content_art);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_slides, container, false);
+        return inflater.inflate(R.layout.fragment_content_art, container, false);
     }
 }
--- app/src/main/java/com/example/sketsaid/MainActivity.java
+++ app/src/main/java/com/example/sketsaid/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.sketsaid;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.widget.*;
-import android.view.*;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import com.example.sketsaid.ui.login.LoginActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String MOVE = null;
@@ -18,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     FragmentBottomNavigation bottomNavigation;;
     FragmentManager manager;
     FragmentSearchTopNavigation searchTopNavigation;
-    FragmentContentSlider contentSlider;
+    FragmentContentArt contentArt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +21,14 @@ public class MainActivity extends AppCompatActivity {
         topNavigation = new FragmentTopNavigation();
         bottomNavigation = new FragmentBottomNavigation();
         searchTopNavigation = new FragmentSearchTopNavigation();
-        contentSlider = new FragmentContentSlider();
+        contentArt = new FragmentContentArt();
 
         //setup the fragments
         manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.fragmentTop, topNavigation);
         transaction.add(R.id.fragmentBottom, bottomNavigation);
-        transaction.add(R.id.fragmentContentSlider, contentSlider);
+        transaction.add(R.id.fragmentContent, contentArt);
         transaction.commit();
     }
 }
